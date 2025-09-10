f144c1aecc7bcea8bf7c67bbe6c9982e7424c5b1
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2025-09-10 09:48:24 +0200

    Subir un maravilloso archivo de actividad guiada

---
+++ KKDVAK.py
@@ -0,0 +1,16 @@
+var1=int(input("introduce un número entero"))
+var2=int(input("introduce otro número entero"))
+var3="hola"
+var4="En un lugar de la Mancha, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor."
+
+total=var1+var2
+
+print("la suma de tus bonitos números ",var1," + ",var2," es ",total)
+#print("el valor de la variable 1 es "+var1)
+##print("el valor de la variable 2 es "+var2)
+##print("el valor de la variable 3 es "+var3)
+##print("el valor de la variable 4 es "+var4)
+
+tarde1="sebas"
+
+print(tarde1," ha llegado tarde el día de hoy")
