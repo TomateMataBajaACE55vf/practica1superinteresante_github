abdef381a6f635c8e751b69f273c19cf9731956f
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2025-09-23 10:49:02 +0200

    ejercicios finalizados

---
+++ Ejercicios Iniciales/Ejercicio18.py
@@ -0,0 +1,9 @@
+#Cines Paradiso celebran su décimo aniversario y por ser un día especial realizan importantes descuentos. A los adultos se les aplicará un 10% de descuento y a los menores de 18 años un 50%. Si la entrada cuesta 12 euros, calcula el total a pagar introduciendo por teclado el número de menores y el número de adultos que asisten al cine.
+numadu=int(input("Introduce un número de adultos: "))
+numniñ=int(input("Introduce un número de menores: "))
+
+adult=numadu*10.8
+niño=numniñ*6
+
+print("El precio total del cine para ",numniñ," menor/es es: ",niñ)
+print("El precio total del cine para ",numadu;" adulto/s es: ",adult)
