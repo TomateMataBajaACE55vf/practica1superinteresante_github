0303bb8a1dbb64f5a946c6e491d83cbb25e007f1
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2025-10-24 15:24:15 +0200

    Update Password1.py

--- ACTIVIDAD PASSWORD/Password1.py
+++ ACTIVIDAD PASSWORD/Password1.py
@@ -22,30 +22,82 @@ if 6 <= passwlen <= 8:
     pass4=password[3]
     pass5=password[4]
     pass6=password[5]
-
     if pass1.isnumeric():
         pass1=float(pass1)
         if 1 >= pass1 >=5:
-            error=str(0)
+            error=error
         else:
             error="Error en el carácter 1 "
     else:
         error="Error en el carácter 1 "
-
     if pass2.isalpha():
         pass2=str(pass2)
         if pass2.islower():
-            if error == 0:
-                print("Maravilloso")
-            else:
-                print(error)
+            error=error
         else:
             error=error+"Error en el carácter 2 "
-            print(error)
-
     else:
         error=error+"Error en el carácter 2 "
-        print(error)
-
+    if pass3.isalpha():
+        pass3=str(pass3)
+        if pass3.islower():
+            error=error
+        else:
+            error=error+"Error en el carácter 3 "
+    else:
+        error=error+"Error en el carácter 3 "
+    if pass4 in ("*@_"):
+        error=error
+    else:
+        error=error+"Error en el carácter 4 "
+    if pass5.isalpha():
+        pass5=str(pass5)
+        if pass5.islower():
+            error=error
+        else:
+            error=error+"Error en el carácter 5 "
+    if pass6.isnumeric():
+        pass6=float(pass6)
+        if 6 >= pass6 >=9:
+            error=error
+        else:
+            error=error+"Error en el carácter 6 "
+    else:
+        error=error+"Error en el carácter 6 "
+    if passwlen == 7:
+        pass7=password[6]
+        if pass7 in ("&/#"):
+            error=error
+        else:
+            error=error+"Error en el carácter 7 "
+        
+        if error == 0:
+            print("El formato del PASSWORD es correcto")
+            breakpoint
+        else:
+            print(error)
+            breakpoint
+    elif passwlen == 8:
+        pass7=password[6]
+        if pass7 in ("&/#"):
+            error=error
+        else:
+            error=error+"Error en el carácter 7 "
+        pass8=password[7]
+        if pass8.isnumeric:
+            pass8=float(pass8)
+            if pass8 >= 5:
+                error=error
+            else:
+                error=error+"Error en el carácter 8"
+        else:
+            error=error+"Error en el carácter 8"
+    else:
+        if error == 0:
+            print("El formato del PASSWORD es correcto")
+            breakpoint
+        else:
+            print(error)
+            breakpoint
 else:
     print(f"Error, el password tiene una longitud de {passwlen} caracteres y no cumple los requisitos")
