e8043027c009eb8c28f4213da4d8728262d5d716
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2025-11-05 09:26:37 +0100

    Update Ejercicio35.py

--- Ejercicios con bucles/Ejercicio35.py
+++ Ejercicios con bucles/Ejercicio35.py
@@ -1,4 +1,5 @@
 #Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
 num=int(input("Introduce el número de veces: "))
+#utilizamos for para realizar un bucle las veces que es el valor de num
 for x in range(num):
     print("Huijing Zheng")
