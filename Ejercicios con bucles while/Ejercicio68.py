9ea5e26eb6bc6f12635bc749649d92c51ace1e01
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2025-12-09 10:16:30 +0100

    Enunciados

--- Ejercicios con bucles while/Ejercicio68.py
+++ Ejercicios con bucles while/Ejercicio68.py
@@ -1,11 +1,4 @@
-#Realiza de nuevo el programa de Password (fase 2). El password debe tener las siguientes consideraciones: 
-#Debe tener una longitud entre 6 y 8 caracteres. 
-#Debe contener como mínimo: 
-#2 números mayores o iguales que 1 y menor o igual que 5 
-#2 letras minúsculas 
-#1 letra mayúscula 
-#2 símbolos (*, _, @, &,/,#) 
-#1 número mayor o igual que 6 y menor o igual que 5
+#Añade al ejercicio anterior la posibilidad de que el programa pregunte si deseas o no continuar introduciendo passwords. Ej. “¿Deseas introducir otro password s/n?
 sino="s"
 while sino == "s":
     num1_5=int(0)
