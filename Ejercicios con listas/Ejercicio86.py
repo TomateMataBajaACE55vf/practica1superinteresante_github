355ed267b22309af3082741deefa3123257e92c0
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2026-01-28 09:53:41 +0100

    Update Ejercicio86.py

--- Ejercicios con listas/Ejercicio86.py
+++ Ejercicios con listas/Ejercicio86.py
@@ -1,10 +1,10 @@
 #Diseñar un programa que al introducir un número calcule la letra correspondiente, teniendo en cuenta los
 #siguientes puntos:
-#Controlar que el valor introducido tenga una longitud de 8 dígitos y sea numérico.
-#Visualizar el NIF completo, concatena la letra correspondiente separado con un guion: ej 11111111-R
-#Controlar aquellas situaciones en que el Resto obtenido de un DNI no aparece en la tabla, por tanto, es
+#Controlar que"El valor introducido tenga una longitud de 8 dígitos y sea numérico.
+#Visualizar"El NIF completo, concatena la letra correspondiente separado con un guion: ej 11111111-R
+#Controlar aquellas situaciones en que"El Resto obtenido de un DNI no aparece en la tabla, por tanto, es
 #incorrecto. Debe aparecer un mensaje de error.
-#Por cada cálculo que se realice, el programa debe ofrecer la opción de continuar o no, introduciendo
+#Por cada cálculo que se realice,"El programa debe ofrecer la opción de continuar o no, introduciendo
 #las letras “s” o “n”. En caso de seleccionar “n” se visualiza MENÚ del apartado 7.
 letras=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
 dnino=[]
@@ -19,7 +19,7 @@ while sino in "Ss":
         tries.append(0)
         nisinino=1
         dnino.append(dni)
-    else:
+    elif len(dni) == 8:
         for x in range(len(dni)):
             if str(dni[x]).isnumeric() == False:
                 print("El valor introducido tiene que ser numérico.")
@@ -37,20 +37,14 @@ while sino in "Ss":
             dni=dni+"-"+letras[int(divi)]
             dnisi.append(dni)
             print(dni) 
+            tries.append(3)
     sino=input("Deseas introducir otro DNI (s/n): ")
     while sino not in "Ss" and sino not in "Nn" or sino.isalnum() == False:
         print("Respuesta incorrecta.")
         sino=input("Deseas introducir otro DNI (s/n): ")
-print("RESULTADOS. Escoge una opción:")
-print("1. Listar NIF correcto ordenado de menor a mayor")
-print("2. Listar DNI incorrecto ordenado de menor a mayor")
-print("3. Número total de errores producidos")
-print("4. Número total de DNI correctos")
-print("5. Porcentajes intentos con error y sin error")
-print("6. Salir s/n")
 nosi=0
-while str(nosi) not in "12345" or nosi not in "SnNn":
-    print("Respuesta inválida")
+final=0
+while final == 0:
     print("RESULTADOS. Escoge una opción:")
     print("1. Listar NIF correcto ordenado de menor a mayor")
     print("2. Listar DNI incorrecto ordenado de menor a mayor")
@@ -59,3 +53,31 @@ while str(nosi) not in "12345" or nosi not in "SnNn":
     print("5. Porcentajes intentos con error y sin error")
     print("6. Salir s/n")
     nosi=input("Introduce una opción: ")
+    if nosi in "SsNn":
+        print("Programa finalizado")
+        final=1
+    if nosi.isnumeric():
+        nosi=float(nosi)
+        final=1
+    if nosi == 1:
+        print(dnisi)
+        final=1
+    if nosi == 2:
+        print(dnino)
+        final=1
+    if nosi == 3:
+        print(len(dnisi))
+        final=1
+    if nosi == 4:
+        print(len(dnino))
+        final=1
+    if nosi == 5:
+        print("El número de intentos es: ",len(tries))
+        print("El % de DNI correctos son: ",round(tries.count(3)/len(tries)*100,1))
+        print("El % de DNI incorrectos son: ",round(tries.count(0,1,2)/len(tries)*100,1))
+        print("El % de DNI con error de longitud es: ",round(tries.count(0)/len(tries)*100,1))
+        print("El % de DNI con error de dígitos es: ",round(tries.count(1)/len(tries)*100,1))
+        print("El % de DNI que no existen es: ",round(tries.count(2)/len(tries)*100,1))
+        final=1
+    if final == 0:
+        print("Respuesta inválida")
