8cfdb64eab777cfa87ed53c39fe6b01420522520
TomateMataBajaACE55vf <huijing.zheng@campus.xaloc.org>
2026-02-24 10:50:25 +0100

    Update Ahorcado.py

--- ACTIVIDAD AHORCADO/Ahorcado.py
+++ ACTIVIDAD AHORCADO/Ahorcado.py
@@ -1,9 +1,11 @@
 import random
+import time
 Lista_palabrasecreta=["palabra","zanguango","competitivo","obtuso","itinerario","longaniza","almendra","matadora","Benidorm","miranda"]
 sino="s"
 while sino in "Ss":
-    aciertos=[]
-    errores=[]
+    tiempo1=time.perf_counter()
+    Lista_aciertos=[]
+    Lista_errores=[]
     Lista_partida=[]
     Lista_ahorcado=["_","_","_","_","_","_","_","_"]
     secret=str(random.choice(Lista_palabrasecreta))
@@ -16,8 +18,11 @@ while sino in "Ss":
     while Lista_ahorcado != ["A","H","O","R","C","A","D","O"] and Lista_partida !=secret:
         pos=-1
         letra=str(input("Introduce una letra: "))
-        while letra.lower() in errores or letra.upper() in errores or letra.lower() in aciertos or letra.upper() in aciertos:
+        while letra.lower() in Lista_errores or letra.upper() in Lista_errores or letra.lower() in Lista_aciertos or letra.upper() in Lista_aciertos:
             print("Esa letra ya la usaste.")
+            letra=str(input("Introduce una letra: "))
+        while len(letra) != 1 or letra.isalpha() == False:
+            print("Lo que has introducido es incorrecto.")
             letra=str(input("Introduce una letra: "))
         if letra.lower() in secret or letra.upper() in secret:
             for y in secret:
@@ -32,13 +37,19 @@ while sino in "Ss":
                     Lista_partida.pop(pos)
                     Lista_partida.insert(pos,y)
             print("[ "+ " , ".join(Lista_partida)+ " ]")
-            aciertos.append(letra)
+            Lista_aciertos.append(letra)
         else:
             error=error+1
             Lista_ahorcado.pop(error)
             Lista_ahorcado.insert(error,["A","H","O","R","C","A","D","O"][error])
             print(*Lista_ahorcado)
-            errores.append(letra)
+            Lista_errores.append(letra)
+    tiempo2=time.perf_counter()
+    print("Aciertos: ",len(Lista_aciertos))
+    print("Errores: ",len(Lista_errores))
+    ttotal=round(tiempo2-tiempo1,0)
+    if 
+    print("Tiempo utilizado: ",int(ttotal)," segundos")
     sino=input("Deseas continuar (s/n): ")
     while sino not in "Ss" and sino not in "Nn" or sino.isalpha() == False:
         print("Respuesta incorrecta.")
